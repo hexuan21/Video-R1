7226d8b70bb016a8b9c3e72f03f6e9d1559092bc
hexuan21 <hex9269@gmail.com>
2025-07-25 07:17:03 +0000

    update

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 from zipfile import ZipFile, BadZipFile
 
 HF_DATASET_USER = "hexuan21"
-HF_DATASET_NAME = "vs2_rl"
+HF_DATASET_NAME = "vs2_rl_data"
 HF_VIDEO_NAME   = "vs2_sft_video"
 CHUNK = 1 << 14  # 16 KB
 
@@ -32,24 +32,23 @@ def download_file(url: str, save_path: str, overwrite: bool = False, timeout: in
 
 
 
-def main(dataset_name: str,data_save_dir: str):
-    data_file  = f"{dataset_name}.json"
+def main(json_name: str, video_zip_name:str, data_save_dir: str):
+    data_file  = f"{json_name}.json"
     data_save = os.path.join(data_save_dir, data_file)
     
-    zip_file  = f"{dataset_name}_videos.zip"
+    video_zip  = f"{video_zip_name}.zip"
     video_dir = os.path.join(data_save_dir, "videos")
-    zip_save = os.path.join(data_save_dir, zip_file)
+    zip_save = os.path.join(data_save_dir, video_zip)
     
     data_url = f"https://huggingface.co/datasets/{HF_DATASET_USER}/{HF_DATASET_NAME}/resolve/main/{data_file}"
-    zip_url = f"https://huggingface.co/datasets/{HF_DATASET_USER}/{HF_VIDEO_NAME}/resolve/main/{zip_file}"
+    video_zip_url = f"https://huggingface.co/datasets/{HF_DATASET_USER}/{HF_VIDEO_NAME}/resolve/main/{video_zip}"
     
     download_file(data_url, data_save, overwrite=True)
     with open(data_save, "r", encoding="utf-8") as f:
         data = json.load(f)
     with open(data_save, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
-    download_file(zip_url, zip_save, overwrite=True)
-
+    download_file(video_zip_url, zip_save, overwrite=True)
 
     os.makedirs(video_dir, exist_ok=True)
     try:
@@ -65,7 +64,8 @@ def main(dataset_name: str,data_save_dir: str):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Prepare RL JSON & ZIP")
-    parser.add_argument("--dataset_name", required=True)
+    parser.add_argument("--json_name", required=True)
+    parser.add_argument("--video_zip_name", required=True)
     parser.add_argument("--data_save_dir", required=True)
     args = parser.parse_args()
-    main(args.dataset_name,args.data_save_dir)
+    main(args.json_name,args.video_zip_name,args.data_save_dir)
--- src/prepare_data.sh
+++ src/prepare_data.sh
@@ -1,5 +1,6 @@
 echo "Download RL data and videos."
-echo "Current RL data name: ${DATASET_NAME}"
-python examples/train_full/prepare_data.py \
-    --data_name ${DATASET_NAME} \
-    --data_save_dir "src/r1-v/data_vs2_grpo" \
+
+python prepare_data.py \
+    --json_name rl_17k \
+    --video_zip_name sft_17k_videos \
+    --data_save_dir "r1-v/data_vs2_grpo" 
