6fe04c25132521e643a93756f4f50d543caffbd0
hexuan21 <hex9269@gmail.com>
2025-07-25 07:19:28 +0000

    update

--- src/prepare_data.sh
+++ src/prepare_data.sh
@@ -1,6 +1,6 @@
 echo "Download RL data and videos."
 
 python prepare_data.py \
-    --json_name rl_17k \
+    --json_name grpo_17k \
     --video_zip_name sft_17k_videos \
     --data_save_dir "r1-v/data_vs2_grpo" 
