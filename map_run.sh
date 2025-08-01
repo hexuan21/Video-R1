0ebb8973fd5eb4224ea235a1043e5d340215c894
hexuan21 <hex9269@gmail.com>
2025-08-01 01:50:19 +0000

    update

--- map_run.sh
+++ map_run.sh
@@ -9,6 +9,10 @@ export MASTER_ADDR=$MLP_WORKER_0_HOST
 export MASTER_PORT=$MLP_WORKER_0_PORT
 export LOCAL_RANK=$(($RANK % $MLP_WORKER_GPU))
 
+export NCCL_BLOCKING_WAIT=1
+export NCCL_ASYNC_ERROR_HANDLING=1
+export NCCL_TIMEOUT=3600
+
 unset https_proxy; unset http_proxy
 export https_proxy="http://100.64.117.161:3128"
 export http_proxy="http://100.64.117.161:3128"
