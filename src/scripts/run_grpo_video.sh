926009f6f0b11fed6f25c29b56977b94ee2e28f8
hexuan21 <hex9269@gmail.com>
2025-08-06 15:45:41 +0000

    update

--- src/scripts/run_grpo_video.sh
+++ src/scripts/run_grpo_video.sh
@@ -17,7 +17,7 @@ OUTPUT_DIR="./log/$RUN_NAME"
 
 wandb login
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node="8" \
+CUDA_VISIBLE_DEVICES=4,5,6,7 torchrun --nproc_per_node="8" \
     --nnodes="1" \
     --node_rank="0" \
     --master_addr="127.0.0.1" \
