c38516de6a219944ce4101e6c93ade989ff9b007
hexuan21 <hex9269@gmail.com>
2025-08-06 05:17:35 +0000

    update

--- src/scripts/run_grpo_video.sh
+++ src/scripts/run_grpo_video.sh
@@ -13,7 +13,7 @@ SFT_Model_Path=videoscore2/vs2_qwen2_5vl_sft_17k_2e-4_2fps_960_720_8192
 DATASET_NAME=./Video-R1-data/grpo_17k.json
 
 RUN_NAME=vs2_qwen2_5vl_grpo_17k_1e-6_base960-720_reward_3_temporal
-OUTPUT_DIR="./log/{$RUN_NAME}"
+OUTPUT_DIR="./log/$RUN_NAME"
 
 wandb login
 
