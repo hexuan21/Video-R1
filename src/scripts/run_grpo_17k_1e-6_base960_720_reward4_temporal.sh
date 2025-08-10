db210e525b8e8b11ca9c884a42a72c5034081967
hexuan21 <hex9269@gmail.com>
2025-08-10 09:08:41 +0000

    update

--- src/scripts/run_grpo_17k_1e-6_base960_720_reward4_temporal.sh
+++ src/scripts/run_grpo_17k_1e-6_base960_720_reward4_temporal.sh
@@ -6,11 +6,11 @@ cd src/r1-v
 # Set temporal to choose between T-GRPO and GRPO, and len_control to enable or disable the length control reward.
 
 # Qwen/Qwen2.5-VL-7B-Instruct
-ID=grpo_17k_5e-7_base768-768_reward3
+ID=grpo_17k_1e-6_base960_720_reward4_temporal
 export DEBUG_MODE="true" # Enable Debug if you want to see the rollout of model during RL
 export LOG_PATH="./debug_log_$ID.txt"
 
-SFT_Model_Path=videoscore2/vs2_qwen2_5vl_sft_17k_2e-4_2fps_768_768_8192
+SFT_Model_Path=videoscore2/vs2_qwen2_5vl_sft_17k_2e-4_2fps_960_720_8192
 DATASET_NAME=./Video-R1-data/grpo_17k.json
 
 RUN_NAME="vs2_qwen2_5vl_$ID"
@@ -18,7 +18,7 @@ OUTPUT_DIR="./log/$RUN_NAME"
 
 wandb login
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node="8" \
+CUDA_VISIBLE_DEVICES=2,3,6,7 torchrun --nproc_per_node="4" \
     --nnodes="1" \
     --node_rank="0" \
     --master_addr="127.0.0.1" \
@@ -32,12 +32,12 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node="8" \
     --max_completion_length 1024 \
     --per_device_train_batch_size 1 \
     --gradient_accumulation_steps 1 \
-    --learning_rate 5e-7 \
+    --learning_rate 1e-6 \
     --lr_scheduler_type "cosine" \
     --weight_decay 0.01 \
     --bf16 \
     --gradient_checkpointing true \
-    --temporal false \
+    --temporal true \
     --len_control true \
     --attn_implementation flash_attention_2 \
     --max_pixels 589824 \
